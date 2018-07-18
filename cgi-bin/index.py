4df8d8db823b9d5d92ff99eb0869abfa6b6061d2
diego95root <minidiego@ono.com>
2018-07-19 01:01:56 +0200

    Minor changes

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -79,37 +79,7 @@ def get_images(query):
     j = json.loads(jsa)
     return j
 
-if (len(form.keys()) == 2):
-    #print html
-    """<img src="https://lumiere-a.akamaihd.net/v1/images/pr_coco_homeentertainment_digialhd_23df6b9d.png?region=0,0,400,400" alt="flash">
-    <img src="http://news.miamisprings.com/wp-content/uploads/2018/05/Coco_poster.png" alt="flash">
-    <img src="https://lumiere-a.akamaihd.net/v1/images/uk_coco_lg_r_02df55ea.png?region=0,0,587,306&width=294" alt="flash">
-    <img src="https://cdn.watch-series.co/cover/coco-hindi.png" alt="flash">
-    <img src="https://pre00.deviantart.net/f009/th/pre/i/2018/085/b/7/crash_trips_coco_by_deannart-dc71hde.png" alt="flash">
-    <img src="https://lumiere-a.akamaihd.net/v1/images/pr_coco_homeentertainment_dvd_47acddd1.png?region=0,0,400,400" alt="flash">
-    <img src="https://static.vix.com/en/sites/default/files/styles/large/public/c/coco-movie.png?itok=6emqdCGB" alt="flash">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Disney%27s_Coco_logo.png/1200px-Disney%27s_Coco_logo.png" alt="flash">
-    <img src="https://fanart.tv/fanart/movies/354912/hdmovieclearart/coco-5a01e6ebbeda6.png" alt="flash">
-    <img src="https://media.vanityfair.com/photos/5a0b835407eff7065a193c51/master/pass/coco-video-billy-elliot-spielberg.png" alt="flash">"""
-
-    orig = form['eng'].value
-    trans = form['rus'].value
-
-    imgs = get_images(orig)
-
-    print html_start, html_end
-
-    for i in range(0, len(imgs["items"])):
-        link = imgs["items"][i]["link"]
-        print '<img src="{}" alt="flash">'.format(link)
-
-    insert_trans(table, orig, trans, link)
-
-
-else:
-    print html_start
-    url = "http://farm9.staticflickr.com/8072/8346734966_f9cd7d0941_z.jpg"
-
+def build_slide(li):
     print '<ul class="slides">' # slider from https://codepen.io/AMKohn/pen/EKJHf (adopted to python)
 
     for i in range(1, 11):
@@ -125,7 +95,7 @@ else:
     		</div>
         </li>
 
-        """.format(i, url, ((i+8)%10)+1, (i % 10) + 1)
+        """.format(i, li[i-1], ((i+8)%10)+1, (i % 10) + 1)
         print one
 
     print '<li class="nav-dots">'
@@ -134,6 +104,28 @@ else:
     print '</li>'
 
     print '</ul>'
+
+if (len(form.keys()) == 2):
+    orig = form['eng'].value
+    trans = form['rus'].value
+
+    imgs = get_images(orig)
+    imgli = []
+
+    for i in range(0, len(imgs["items"])):
+        link = imgs["items"][i]["link"]
+        imgli.append(link)
+        #print '<img src="{}" alt="flash">'.format(link)
+
+    print html_start
+    build_slide(imgli)
+    print html_end
+
+    #insert_trans(table, orig, trans, link)
+
+
+else:
+    print html_start
     print html_end
     see_all()
     #delete_trans(table, "hello")
